197d2485c3ad21f7e6158da7149d5de6fb6de64d
Karol Lassak <karol.lassak@coi.gov.pl>
2014-06-12 16:59:32 +0200

    Version working with stash
    - with small help of other plugins

--- src/main/java/org/eu/ingwar/maven/sputnik/SputnikMojo.java
+++ src/main/java/org/eu/ingwar/maven/sputnik/SputnikMojo.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2014 Ingwar & co..
  *
@@ -16,13 +15,17 @@
  */
 package org.eu.ingwar.maven.sputnik;
 
+import java.util.Properties;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import pl.touk.sputnik.configuration.Configuration;
+import pl.touk.sputnik.review.Engine;
 
 /**
  *
@@ -30,15 +33,72 @@ import org.apache.maven.plugins.annotations.Parameter;
  */
 @Mojo(name = "sputnik",
         defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST,
-        threadSafe = true
+        threadSafe = true,
+        aggregator = true
         )
-@Execute()
 public class SputnikMojo extends AbstractMojo {
-    @Parameter(name = "test", defaultValue = "acham", property = "ala.ma.test")
-    private String test;
+
+    @Parameter(property = "sputnik.connector", defaultValue = "stash", required = true)
+    private String connector;
+
+    @Parameter(property = "sputnik.pullRequestId", required = true)
+    private String pullRequestId;
     
+    @Parameter(property = "sputnik.checkstyle.enabled", defaultValue = "true")
+    private String checkstyleEnabled;
+    
+    @Parameter(property = "sputnik.checkstyle.configurationFile")
+    private String checkstyleConfigurationFile;
+    
+    
+    @Parameter(property = "sputnik.stash.host")
+    private String stashHost;
+    
+    @Parameter(property = "sputnik.stash.port", defaultValue = "80")
+    private String stashPort;
+    
+    @Parameter(property = "sputnik.stash.useHttps", defaultValue = "false")
+    private String stashUseHttps;
+    
+    @Parameter(property = "sputnik.stash.username")
+    private String stashUsername;
+
+    @Parameter(property = "sputnik.stash.password")
+    private String stashPassword;
+
+    @Parameter(property = "sputnik.stash.projectKey")
+    private String stashProjectKey;
+
+    @Parameter(property = "sputnik.stash.repositorySlug")
+    private String stashRepositorySlug;
+
+
+    @Parameter(property = "project", readonly = true)
+    private MavenProject mavenProject;
+    
+    @Parameter(property = "mojoExecution", readonly = true)
+    private MojoExecution execution;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        Configuration conf = Configuration.instance();
+        Properties prop = new Properties();
+        
+        prop.setProperty("cli.connector", connector);
+        prop.setProperty("cli.pullRequestId", pullRequestId);
+        
+        prop.setProperty("checkstyle.enabled", checkstyleEnabled);
+        prop.setProperty("checkstyle.configurationFile", checkstyleConfigurationFile);
+        
+        prop.setProperty("stash.host", stashHost);
+        prop.setProperty("stash.port", stashPort);
+        prop.setProperty("stash.useHttps", stashUseHttps);
+        prop.setProperty("stash.username", stashUsername);
+        prop.setProperty("stash.password", stashPassword);
+        prop.setProperty("stash.projectKey", stashProjectKey);
+        prop.setProperty("stash.repositorySlug", stashRepositorySlug);
+        
+        conf.setProperties(prop);
+        new Engine().run();
     }
-    
 }
